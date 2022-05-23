56f1c0f1fb13fc4e262d68f91a030dd1c57f4800
Rémi Huguet <remi.huguet@gmail.com>
2022-05-23 09:39:50 +0200

    Live 15 - Profits and losses v1

--- transactions/views.py
+++ transactions/views.py
@@ -5,11 +5,36 @@ from .models import Transaction
 
 @require_http_methods(["GET"])
 def profitsandlosses(request):
-    transactions = Transaction.objects.all().order_by("category", "sub_category")
+    transactions = Transaction.objects.all().order_by(
+        "category", "sub_category", "label"
+    )
     months = transactions.dates("date", "month")
+    ref_months = [f"{m.year}{m.month}" for m in months]
+    p_and_l = []
+    line = None
+    for transac in transactions:
+        if not line or transac.label != line["label"]:
+            if line:
+                p_and_l.append(line)
+            line = {
+                "label": transac.label,
+                "category": transac.get_category_display(),
+                "sub_category": transac.sub_category.name
+                if transac.sub_category
+                else "",
+            }
+            line["amounts"] = ["" for month in ref_months]
+
+        line["amounts"][
+            ref_months.index(f"{transac.date.year}{transac.date.month}")
+        ] = transac.total_amount
 
     return render(
         request,
         "profits_and_losses.html",
-        {"months": months, "transactions": transactions},
+        {
+            "months": months,
+            "pandl": p_and_l,
+            "ref_months": ref_months,
+        },
     )
