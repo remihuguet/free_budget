df4480f34a13e184ef0f98d63a756c7daf558334
Rémi Huguet <remi.huguet@gmail.com>
2022-06-15 11:43:31 +0200

    Live 19 - Heroku config ALLOWED_HOSTS

--- budget/settings.py
+++ budget/settings.py
@@ -5,16 +5,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0spawchj%2e459bwiko$tm7c0+#7q@-=l&&e+4l4p!4^s%^sl("
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*.herokuapp.com"]
 
 
 # Application definition
